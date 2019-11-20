f6957f9a632a2eedbdae37ec7ef6d4f14e9a7b76
Joaquin Gutierrez <joa18gm@gmail.com>
2019-11-20 08:05:47 -0500

    Ejercicio2

---
+++ E2.py
@@ -0,0 +1,18 @@
+import os
+os.system("cls")
+
+def contraletra (texto,letra):
+    for letra in texto:
+        if letra.count(texto)==0:
+            print(letra)
+    return contraletra               
+
+
+a= contraletra("holamundo", "o")
+print(a)
+
+b= contraletra("123456", "2")
+print(b)
+
+c= contraletra("lapractica", "a")
+print(c)
---
+++ E3.py
@@ -0,0 +1,41 @@
+import os
+os.system("cls")
+
+a=[]
+b=[]
+c=[]
+cant1=0
+cant2=0
+while len(a)<3 and len(b)<3 and len(c)<3:
+
+    nombre=input("Ingrese su nombre: ")
+    a.append(nombre)
+    sexo = input("Ingrese su sexo: ")
+    if sexo=="masculino" or sexo =="femenino":
+        b.append(sexo)
+        if sexo=="masculino":
+            cant1= cant1+1
+        else:
+            cant2=cant2+1    
+    else:
+        print("Especifique sexo ya sea masculino o femenino. ")        
+    edad = int(input("Ingrese su edad: "))
+    if edad >=5 and edad <=17:
+        c.append(edad)
+    else:
+        print("Edad no en rango. ")    
+        
+s=0
+for i in c:
+    s=s+i
+
+print("Las personas inscritas son: ")
+print(a)
+print(b)
+print(c)
+print("")
+print("La cantidad de hombres son:"+ str(cant1))
+print("")
+print("La cantidad de mujeres son:"+ str(cant2))
+print("")
+print("La edad promedio de personas registradas son: "+ str(s))
