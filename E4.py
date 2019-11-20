3418b80122474ff3b24946e5047ea69d5f73793c
Joaquin Gutierrez <joa18gm@gmail.com>
2019-11-20 08:40:39 -0500

    ejer4

---
+++ E4.py
@@ -0,0 +1,18 @@
+import os, random
+os.system("cls")
+
+
+nmatriz=int(input("Ingrese el numero de orden de la matriz: "))
+if 2 < nmatriz < 5:
+    numeroaleatorio=random.randint(1,100)
+
+for i in a1 and a2 and a3
+a1=[[],[],[]]
+a2=[[],[],[]]
+a3=[[],[],[]]
+a1.append(numeroaleatorio)
+a2.append(numeroaleatorio)
+a3.append(numeroaleatorio)
+
+print(a1)
+print(a2)
